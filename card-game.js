ad7daf8de6e4c46d2a00afc96e675bbe415a7e5d
Anish Ojha <anishojha82@gmail.com>
2020-01-28 17:41:56 +0530

    added functionalities for the game

--- card-game.js
+++ card-game.js
@@ -212,5 +212,198 @@ function detectWinner(item) {
     }
 }
 
+/*
+  First victory condition function
+  -The array is passed to check if one of the players have all
+   the cards with same rank value.The player found is the winner
+   If there are more than one player found with the victory condition
+   then checkTieWinner() is called with condition passed as
+   "allEqual" otherwise the function returns the player name who wins the game.
+*/
+function checkEqual(item) {
+    let handArr = [];
+    let players = [];
+    item.map((el, i) => {
+        el.hand.map(val => {
+            handArr.push(val);
+            if (handArr.length === cards) {
+                if (_.differenceBy(handArr[0], handArr[1], handArr[2], 'value') === []) {
+                    players.push({ name: el.name, i });
+                    handArr = [];
+                }
+                else {
+                    handArr = [];
+                }
+            }
+        });
+    });
+    if (players.length > 1) return checkTieWinner(players, 'allEqual');
+    if (players.length > 0) return players[0].name;
+}
+
+//If first fails
+/*
+  Second victory condition function
+  -The array is passed to check if one of the players have
+   the cards with their value in sequence.(for ex a player with 3 cards have card sequence as 4, 5, 6)
+   The player found is the winner If there are more than one player found with the victory condition
+   then checkTieWinner() is called with condition passed as
+   "sequence" otherwise the function returns the player name who wins the game.
+*/
+function checkSequence(item) {
+    let weightArr = [];
+    let players = [];
+    item.map((el, i) => {
+        el.hand.map(val => {
+            weightArr.push(val.weight);
+            if (weightArr.length === cards) {
+                const diffArr = weightArr.slice(1).map((n, i) => weightArr[i] - n);
+                Math.abs(diffArr);
+                const isDiff = diffArr.every(val => val == 1);
+                if (isDiff) players.push({ name: el.name, i });
+                weightArr = [];
+            }
+        });
+    });
+    if (players.length > 1) return checkTieWinner(players, 'sequence');
+    if (players.length > 0) return players[0].name;
+}
+
+//If second fails
+/*
+  Third victory condition function
+  -The array is passed to check if one of the players have
+   two similar cards (for ex two 'K' or 'A').The player found is the winner
+   If there are more than one player found with the victory condition
+   then checkTieWinner() is called with condition passed as
+   "pair" otherwise the function returns the player name who wins the game.
+*/
+function checkPair(item) {
+    let pairArr = [];
+    let players = [];
+    item.map((el, i) => {
+        el.hand.map(val => {
+            pairArr.push(val.value);
+            if (pairArr.length === cards) {
+                const unique = checkIfUniqueArr(pairArr);
+                if (!unique) players.push({ name: el.name, i });
+                pairArr = [];
+            }
+        });
+    });
+    if (players.length > 1) return checkTieWinner(players, 'pair');
+    if (players.length > 0) return players[0].name;
+}
+
+//If all condition fail
+/*
+  Fourth victory condition function
+  -The array is passed to check the top card of each player.
+   The player with a top and unique card found is the winner
+   If there are more than one player found with the victory condition
+   then checkTopCardTie() is called with tied players and top card 
+   values passes as params
+   for ex if there are 4 players with 3 cards each
+   Player 1 - 'A', '2' ,'3'
+   Player 2 - 'K', '1' ,'10'
+   Player 3 - 'A', '5' ,'0'
+   Player 4 - 'Q', '7' ,'9'
+   we can see that top cards for each player is A, K, A, Q
+   but in this case we also see a tie between two players - 1 & 3
+   so checkTopCardTie is called to break the tie
+   ***if the case was A, K, K, Q - although the array contains tie but
+   the winner is the player having card 'A'***
+   Array duplicacy is checked with the help of checkIfUniqueArr()
+*/
+
+function checkTopCard(item) {
+    let topCardArr = [];
+    let values = [];
+    let max = 0;
+    let count = 0;
+    item.map((el, i) => {
+        el.hand.map(val => {
+            max = Math.max(max, val.weight);
+            count++;
+            if (count === cards) {
+                topCardArr.push({ max, i });
+                count = 0;
+                max = 0;
+            }
+        });
+    });
+    const unique = checkIfUniqueArr(topCardArr.map(el => el.max));
+    if (!unique) {
+        topCardArr.map(el => values.push(el.max));
+        max = Math.max(...values);
+        var map = new Map();
+        topCardArr.forEach(val => map.set(val.max, (map.get(val.max) || 0) + 1));
+        topCardArr = topCardArr.filter(val => map.get(val.max) > 1);
+        if (max > topCardArr[0].max) {
+            return values.indexOf(max);
+        }
+        else {
+            return checkTopCardTie(topCardArr);
+        }
+    } else {
+        topCardArr.map(el => values.push(el.max));
+        max = Math.max(...values);
+        return values.indexOf(max);
+    }
+}
+
+//Function to check if array contains duplicates
+function checkIfUniqueArr(arr) {
+    return arr.length === new Set(arr).size;
+}
+
+/*
+ checkTopCardTie()
+ -This function checks for the tie breaker between the players
+ for ex if 4 players have top cards of 'A','A','K','K'
+ then there is also a tie breaker and also we have the max or the top rank
+ value card for the tie breaker, in this case 'A','A' will go for the tie 
+ breaker and then the rest of the game will follow.
+ The functions handles all these corner cases
+*/
+function checkTopCardTie(arr) {
+    let tieCards = [];
+    let max = 0;
+    arr.map(el => tieCards.push(el.max));
+    max = Math.max(...tieCards);
+    arr = arr.filter(val => val.max == max);
+    return dealTie(arr);
+}
+
+/*
+ dealTie()
+ -After all the cases for players have been checked and the tied players are 
+ sorted and filtered according the criteria , then this function is called in
+ the end to determine the winner.
+ Each tied player has to start a new game with  a new card given to him
+ The game goes on until a top card is found
+*/
+function dealTie(arr) {
+    let tiePlayers = [];
+    let max = 0;
+    while (true) {
+        let tieWeight = [];
+        for (let x = 0; x < arr.length; x++) {
+            let card = deck.pop();
+            tiePlayers.push(players[arr[x].i]);
+            tiePlayers[x].hand = [];
+            tiePlayers[x].hand.push(card);
+        }
+        tiePlayers.map(val => val.hand.map(el => tieWeight.push(el.weight)));
+        const unique = checkIfUniqueArr(tieWeight);
+        if (unique) {
+            max = Math.max(...tieWeight);
+            return tieWeight.indexOf(max);
+        }
+        else {
+            continue;
+        }
+    }
+}
 
 
